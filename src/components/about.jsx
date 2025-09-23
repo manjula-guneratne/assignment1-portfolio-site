4fa90086eba013bfc194ef5f380cf36b5906de81
Manjula Guneratne <mguneratne3@gmail.com>
2025-09-22 22:45:43 -0400

    image added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,17 @@ function About() {
     return (
         <div>
             <h1>About Me</h1>
-            <p>This is the About Me page.</p>
+
+            <div class="d-flex justify-content-md-center">
+                <div class="flex-shrink-0">
+                    <img src="" alt="..."/>
+                </div>
+                <div class="flex-grow-1 ms-3">
+                    This is some content from a media component. You can replace this with any content and adjust it as needed.
+                </div>
+            </div>
+
+            <p><a href="https://docs.google.com/document/d/14va3Vk7lsrwfy7Lf2inikEqJVZyLk4Wd9giwjgRBy1A/edit?usp=sharing">Resume Link</a></p>
         </div>
     ); 
 }
