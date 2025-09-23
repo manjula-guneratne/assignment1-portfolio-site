e9478ce27bb30cc2595fda49823b7d92f5d999da
Manjula Guneratne <mguneratne3@gmail.com>
2025-09-23 07:36:35 -0400

    portfolio image resized

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React from "react";
 
 function About() {
-    return (
-        <div>
-            <h1>About Me</h1>
+  return (
+    <div>
+      <h1>About Me</h1>
 
-            <div class="d-flex justify-content-md-center">
-                <div class="flex-shrink-0">
-                    <img src="/images/Portfolio_image.jpg" alt="My portfolio image"/>
-                </div>
-                <div class="flex-grow-1 ms-3">
-                    This is some content from a media component. You can replace this with any content and adjust it as needed.
-                </div>
-            </div>
-
-            <p><a href="https://docs.google.com/document/d/14va3Vk7lsrwfy7Lf2inikEqJVZyLk4Wd9giwjgRBy1A/edit?usp=sharing">Resume Link</a></p>
+      <div class="d-flex justify-content-md-center">
+        <div class="flex-shrink-0">
+          <img
+            src="/images/Portfolio_image.jpg"
+            alt="My portfolio image"
+            width="50%"
+            height="100%"
+          />
         </div>
-    ); 
+        <div class="flex-grow-1 ms-3">
+          Name: Manjula Guneratne
+        </div>
+      </div>
+
+      <p>
+        <a href="https://docs.google.com/document/d/14va3Vk7lsrwfy7Lf2inikEqJVZyLk4Wd9giwjgRBy1A/edit?usp=sharing">
+          Resume Link
+        </a>
+      </p>
+    </div>
+  );
 }
 export default About;
