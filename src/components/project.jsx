01c68da7adf019a6b588577d87cc9867579bfa6c
Manjula Guneratne <mguneratne3@gmail.com>
2025-09-24 05:32:33 -0400

    links and cards complete

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -5,13 +5,14 @@ function Project() {
     <>
       <div>
         <h1>My Projects</h1>
-        <p>This is the Projects page.</p>
+        <p>Click on the Card to Visit the project</p>
       </div>
       <div className="container">
-        <div className="row">
+        <div className="row no-gutters">
           <div className="col">
-            <div className="card" style={{ width: "18rem" }}>
-              <img src="..." className="card-img-top" alt="..." />
+            <a href="https://dog-dash.herokuapp.com/" target="_blank" rel="..." className="...">
+            <div className="card h-100 w-100" style={{ width: "18rem" }}>
+              <img src="/images/dog_dash.png" className="card-img-top" alt="..." />
               <div className="card-body">
                 <p className="card-text">
                   This is a web application for seeking dog walker. Even though
@@ -25,10 +26,12 @@ function Project() {
                 </p>
               </div>
             </div>
+            </a>
           </div>
           <div className="col">
-            <div className="card" style={{ width: "18rem" }}>
-              <img src="..." className="card-img-top" alt="..." />
+            <a href="https://the-coders-bay-app.herokuapp.com/" target="_blank" rel="..." className="...">
+            <div className="card h-100 w-100" style={{ width: "18rem" }}>
+              <img src="/images/coders_bay.png" className="card-img-top" alt="..." />
               <div className="card-body">
                 <p className="card-text">
                   The Coders Bay is an online forum where like minded
@@ -40,10 +43,12 @@ function Project() {
                 </p>
               </div>
             </div>
+            </a>
           </div>
           <div className="col">
-            <div className="card" style={{ width: "18rem" }}>
-              <img src="..." className="card-img-top" alt="..." />
+            <a href="https://uot-project1-group07.github.io/project1-group07/" target="_blank" rel="..." className="...">
+            <div className="card h-100 w-100" style={{ width: "18rem" }}>
+              <img src="/images/covid_positivity.png" className="card-img-top" alt="..." />
               <div className="card-body">
                 <p className="card-text">
                   A site to help individuals who are self isolating due to covid
@@ -52,6 +57,7 @@ function Project() {
                 </p>
               </div>
             </div>
+            </a>
           </div>
         </div>
       </div>
