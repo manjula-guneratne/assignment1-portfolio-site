4a88093a5c55930204f5e5d1f7d7dba4e8a631dc
Manjula Guneratne <mguneratne3@gmail.com>
2025-09-24 04:56:57 -0400

    card framework added

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,12 +1,51 @@
-import react from 'react';
+import react from "react";
 
 function Project() {
-    return (
-        <div>
-            <h1>My Projects</h1>
-            <p>This is the Projects page.</p>
+  return (
+    <>
+      <div>
+        <h1>My Projects</h1>
+        <p>This is the Projects page.</p>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-    ); 
+      </div>
+    </>
+  );
 }
 
 export default Project;
