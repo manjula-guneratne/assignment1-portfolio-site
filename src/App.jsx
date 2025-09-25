ed9373f7c1a2b09ab4c3a5167c0ebdbc636a4693
Manjula <mguneratne3@gmail.com>
2025-09-25 10:38:57 -0400

    corrected the react import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 // import puppy from "./images/puppy.png";
@@ -11,7 +8,6 @@ import Services from "./components/services";
 import Contact from "./components/contact";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
